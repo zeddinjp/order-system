6a4b95c8554681a2dba5b89585ed23ccfac24b26
zeddinjp <zeddinjp@gmail.com>
2026-06-14 22:29:55 +0800

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: '訂單管理系統',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="zh-TW">
+      <body>{children}</body>
+    </html>
+  );
+}
